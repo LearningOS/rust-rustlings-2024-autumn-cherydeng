50430352dc8e4d4c5207f05356232b42112346d1
cherydeng <1053694683@qq.com>
2024-10-08 18:12:07 +0800

    update

--- exercises/lifetimes/lifetimes1.rs
+++ exercises/lifetimes/lifetimes1.rs
@@ -8,9 +8,9 @@
 // Execute `rustlings hint lifetimes1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM DONE
 
-fn longest(x: &str, y: &str) -> &str {
+fn longest<'A>(x: &'A str, y: &'A str) -> &'A str {
     if x.len() > y.len() {
         x
     } else {
