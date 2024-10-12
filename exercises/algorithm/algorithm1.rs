56be3c0ab02384704848fb5b9f6d4d349faa2c32
cherydeng <1053694683@qq.com>
2024-10-12 18:06:21 +0800

    update

--- exercises/algorithm/algorithm1.rs
+++ exercises/algorithm/algorithm1.rs
@@ -2,7 +2,7 @@
 	single linked list merge
 	This problem requires you to merge two ordered singly linked lists into one ordered singly linked list
 */
-// I AM NOT DONE
+// I AM DONE
 
 use std::fmt::{self, Display, Formatter};
 use std::ptr::NonNull;
@@ -70,13 +70,43 @@ impl<T> LinkedList<T> {
         }
     }
 	pub fn merge(list_a:LinkedList<T>,list_b:LinkedList<T>) -> Self
-	{
-		//TODO
-		Self {
-            length: 0,
-            start: None,
-            end: None,
+    where
+        T: Ord + Clone,
+    {
+		let mut merge_list = LinkedList::<T>::new();
+        
+        let mut node_a = list_a.start;
+        let mut node_b = list_b.start;
+
+        while node_a.is_some() || node_b.is_some(){
+            if node_a.is_some() && node_b.is_some(){
+                unsafe{
+                    if (*node_a.unwrap().as_ptr()).val <= (*node_b.unwrap().as_ptr()).val {
+                        merge_list.add((*node_a.unwrap().as_ptr()).val.clone());
+                        node_a = (*node_a.unwrap().as_ptr()).next;
+                    }
+                    else{
+                        merge_list.add((*node_b.unwrap().as_ptr()).val.clone());
+                        node_b = (*node_b.unwrap().as_ptr()).next;
+                    }
+                }
+            }
+            else if node_a.is_some(){
+                unsafe{
+                    merge_list.add((*node_a.unwrap().as_ptr()).val.clone());
+                    node_a = (*node_a.unwrap().as_ptr()).next;
+                }
+            }
+            else if node_b.is_some(){
+                unsafe{
+                    merge_list.add((*node_b.unwrap().as_ptr()).val.clone());
+                    node_b = (*node_b.unwrap().as_ptr()).next;
+                }
+            }
         }
+            
+
+        merge_list
 	}
 }
 
